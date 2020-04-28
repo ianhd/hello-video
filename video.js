595deba89b6e7288e76c6f152b86c7249715a20e
Ian Davis <ihdavis@gmail.com>
2020-04-28 18:33:29 -0400

    progress

--- video.js
+++ video.js
@@ -2,22 +2,21 @@ class Video {
     constructor(elementId, options) {
         this.elId = elementId;
         this.element = document.getElementById(elementId);
+        this.multipleVideos = options.sources.length > 1;
         
         if (options.autoplay) this.element.setAttribute("autoplay","");
         this.setSource(options.sources);
     }
     setSource(sources) {
-        if (sources.length === 1) {
-            this.element.setAttribute("src", sources[0].src);
-            return;
-        }
-        // more than 1 source
-        console.log('setSource and elId is ' + this.elId);
+        this.element.setAttribute("src", sources[0].src);
     }
 }
 var vidOptions = {
     autoplay: false,
     sources: [
+        {
+            src: 'https://zcast.swncdn.com/episodes/zcast/gateway-church/2020/04-26/816286/1046_202042445030-c.mp4' // clip
+        },
         { 
             src: 'https://zcast.swncdn.com/episodes/zcast/greg-laurie-tv/2020/01-05/801572/802_2020128121316.mp4'
         }
